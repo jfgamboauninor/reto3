e8587e1c943006b116d40c64a63bb3162a3222f3
jfgamboauninor <jfgamboa@uninorte.edu.co>
2022-12-01 22:30:48 -0500

    version3

---
+++ README.md
@@ -0,0 +1,15 @@
+# chat
+Solucion planteada para el reto 3 del ciclo 4
+
+Un template para una aplicación de chat usando la base de datos de tiempo real de firebase
+
+Video: https://youtu.be/B3SfN_NcBjk
+
+Equipo 11 del 2287
+
+Integrantes:
+Javier Fernando Gamboa Torres
+Danilo José Gómez Comas
+Jhonnys de Avila
+Link del video:
+
---
+++ lib/config/configuration.dart
@@ -0,0 +1,13 @@
+//create a file called configuration.dart on this folder
+// copy the values from firebase
+
+class Configuration {
+  static const apiKey = "AIzaSyBo3ZvZMetyOXyj8QiFz17Y-C1cZBznVoM";  // tomado de current_key
+  static const authDomain = "chatuno-51c43.firebaseapp.com"; // tomado de oauth_client
+  static const databaseURL = "hhttps://chatuno-51c43-default-rtdb.firebaseio.com"; // tomado de firebase_url
+  static const projectId = "chatuno-51c43"; // tomado de project_id
+  static const storageBucket = "chatuno-51c43.appspot.com"; // tomado de storage_bucket
+  static const messagingSenderId = "805104029168"; //no lo vamos a usar
+  static const appId = "1:805104029168:web:3e7ca67e629e96c159e135"; // tomado de mobilesdk_app_id
+  static const measurementId = "G-YQ4HR3K4S0"; //no lo vamos a usar
+}
